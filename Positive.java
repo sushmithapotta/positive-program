745c38660868b83560b23038d7595a122f9ee52c
sushmithapotta <pottasushmitha9@gmail.com>
2025-01-30 22:23:16 +0530

    Add files via upload

---
+++ Positive.java
@@ -0,0 +1,16 @@
+public class Positive {
+    public static void main(String[] args) {
+        int a=-15;
+        if(a>0){
+            System.out.println("positive");
+        }
+        if(a==0){
+            System.out.println("zero");
+        }
+        if(a<0){
+            System.out.println("negative");
+        } 
+
+    }
+}
+
